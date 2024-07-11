6b2dec20384e543fbbe1c59a56e9a4b71c006131
keerati <keerati9341@gmail.com>
2024-07-11 20:38:15 +0700

    commit 8.7

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-// defineProps<{
-//  msg: string
-// }>()
-const event = ref({
-  id: 5928101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'Janury 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Ket Laydee'
-})
+import type { Event } from '@/type'
+defineProps<{
+  event: Event
+}>()
+// const event = ref({
+//   id: 5928101,
+//   category: 'animal welfare',
+//   title: 'Cat Adoption Day',
+//   description: 'Find your new feline friend at this event.',
+//   location: 'Meow Town',
+//   date: 'Janury 28, 2022',
+//   time: '12:00',
+//   petsAllowed: true,
+//   organizer: 'Ket Laydee'
+// })
 </script>
 
 <template>
@@ -37,4 +38,5 @@ const event = ref({
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0,0,0,0.2);
 }
+
 </style>
---
+++ 331-intro-to-vite/src/components/EventInfo.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Event from '@/types/Event'
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+    <div class="event-class">
+    <div class="event-info">
+        <span>{{ event.category }}</span>
+        <span>{{ event.organizer }}</span>      
+    </div>        
+    </div>
+
+  </template>
+  
+  <style scoped>
+  .event-info {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+    text-align: right;
+    font-size: 20px;
+  }
+  .event-info:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  }
+  .event-info span{
+    font-size: 12px;
+  }
+  </style>
