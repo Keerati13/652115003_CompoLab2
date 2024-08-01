fcf6045602d83e26c99394291dc5e6b719ebf4ee
keerati <keerati9341@gmail.com>
2024-08-02 01:54:51 +0700

    commit L3B 3.2

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
 import StudentView from '@/views/StudentView.vue'
-import EventDetailView from '@/views/EventDetailView.vue'
+import EventDetailView from '@/views/event/DetailView.vue'
+import EventRegisterView from '@/views/event/RegisterView.vue'
+import EventEditView from '@/views/event/EditView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,11 +15,31 @@ const router = createRouter({
       component: EventListView,
       props: (route) => ({ page: parseInt(route.query.page?.toString() || '1') })
     },
-    {
+    { 
       path: '/event/:id',
-      name: 'event-detail-view',
+      name: 'event-layout-view',
       component: EventDetailView,
-      props: true
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
+        }
+      ]
     },
     {
       path: '/about',
