79609e3fa05921fc8ace39c7cafcbd82f25f834d
keerati <keerati9341@gmail.com>
2024-08-11 18:19:28 +0700

    commit L5B 1.6

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from '@/stores/message';
 import { storeToRefs } from 'pinia';
+
 const store = useMessageStore()
 const { message } = storeToRefs(store)
 </script>
