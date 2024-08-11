02a9712e97b9c2e135ee46650308163b8e7dad7c
keerati <keerati9341@gmail.com>
2024-08-12 02:36:44 +0700

    commit L5B 11.2

--- 331-intro-to-vite/src/views/AboutView.vue
+++ 331-intro-to-vite/src/views/AboutView.vue
@@ -4,12 +4,12 @@
   </div>
 </template>
 
-<style>
-/* @media (min-width: 1024px) {
+<!-- <style>
+@media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-} */
-</style>
+}
+</style> -->
