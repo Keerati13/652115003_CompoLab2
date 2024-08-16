a2580fb7302886976826033d19f35f82b56d3549
keerati <keerati9341@gmail.com>
2024-08-16 11:14:37 +0700

    13.6

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <template>  
+<SpeedInsights/>
   <div class="text-center font-sans text-gray-700 antialias">
     <header>
       <div class="wrapper">
